1d3b70d9c5084cae4de42c7b9687269c769fcb52
tyoprataama <tyoprataama004@gmail.com>
2024-02-29 22:30:51 +0700

    refactor: mainpage components

---
+++ src/components/layout/Main/index.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+
+const MainPage = () => {
+  return (
+    <div className="flex xl:items-start lg:text-start xl:text-start text-center flex-col xl:px-9 xl:py-[100px] lg:px-9 lg:py-[100px] md:px-[120px] px-[20px] py-[50px] lg:gap-5 gap-4">
+      <h1 className="text-4xl lg:text-5xl xl:text-5xl font-bold lg:leading-snug leading-tight xl:w-[60%]">
+        <span className="text-blue-500">Digitalisasi</span>
+        <br />
+        <span>Bisnis Anda</span>
+        <br />
+        <span>Sekarang Juga!</span>
+      </h1>
+      <p className="text-gray-500 dark:text-gray-300 xl:w-[80%] xl:text-base text-sm">
+        Nukuweb menawarkan Anda untuk dapat mengembangkan sektor digital yang
+        tangguh. Ciptakan website yang eksklusif untuk bisnis Anda, kembangkan
+        aplikasi mobile untuk memperluas jangkauan, dan susun sistem internal
+        terintegrasi demi optimalisasi operasional bisnis Anda sendiri!.
+      </p>
+      <div className="flex w-full justify-center xl:justify-start lg:justify-start">
+        <Button variant={"main"} className="md:w-[50%]">
+          Lihat lebih lanjut
+        </Button>
+      </div>
+      <div className="welcome-code xl:mt-5 w-[100%] h-[300px] xl:h-[410px]"></div>
+    </div>
+  );
+};
+
+export default MainPage;
